74f8927b528be458cf173e7083d8cf428c9e3b8b
TheRealFonicsDE <145944402+TheRealFonicsDE@users.noreply.github.com>
2023-12-11 20:54:28 +0100

    FonicsCodingPHPMyAdminInstall.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#    Copyright (C) 2021  Julian G.
+#    Copyright (C) 2023 by FonicsCoding
 #
 #    This program is free software; you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License 2 as published by
